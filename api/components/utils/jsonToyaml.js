6fc41ddb804515c68d8e95ef9aaceb6b1ca40900
vishal-ondc <vishal.chaubey@ondc.org>
2025-07-21 03:45:11 +0530

    fix: inhance the assaying service

--- api/components/utils/jsonToyaml.js
+++ api/components/utils/jsonToyaml.js
@@ -6,7 +6,7 @@ const { log } = require("console");
 // Define the root directory where your JSON files are located
 const rootDirectory = path.join(
   __dirname,
-  "../Examples/Services_home_service_yaml/on_select"
+  "../Examples/Agriculture_services_yaml"
 );
 
 // Function to convert a JSON file to YAML
